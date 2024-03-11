0594d97fed0808feca2a628a0513e4ca28af17c2
Jin-Seoung <jinseoungoh@gmail.com>
2024-03-11 10:42:29 +0900

    Create llm.llm.py

---
+++ RAG/Network_Analysis_through_LLMs_for_Knowledge_Extraction/llm.llm.py
@@ -0,0 +1,51 @@
+from src.logger import get_console_logger
+from src.llm.prompts import PROMPTS
+
+
+logger = get_console_logger("llm")
+MIND_MAP_EXTRACTION_MODEL = "gpt-4-turbo-preview"
+MIND_MAP_INSPECTION_MODEL = "gpt-4"
+
+def extract_mind_map_data(openai_client: object, text: str) -> None:
+    logger.info(f"Extracting mind map data from text...")
+    response = openai_client.chat.completions.create(
+        model=MIND_MAP_EXTRACTION_MODEL,
+        response_format={"type": "json_object"},
+        temperature=0,
+        messages=[
+            {"role": "system", "content": PROMPTS["mind_map_of_one"]},
+            {"role": "user", "content": f"{text}"},
+        ],
+    )
+    return response.choices[0].message.content
+
+
+def extract_mind_map_data_of_two(
+    openai_client: object, source_text: str, target_text: str
+) -> None:
+    logger.info(f"Extracting mind map data from two texts...")
+    user_prompt = PROMPTS["mind_map_of_many"].format(
+        source_text=source_text, target_text=target_text
+    )
+    response = openai_client.chat.completions.create(
+        model=MIND_MAP_INSPECTION_MODEL,
+        response_format={"type": "json_object"}, # this is very important!
+        messages=[
+            {"role": "system", "content": PROMPTS["mind_map_of_many"]},
+            {"role": "user", "content": user_prompt},
+        ],
+    )
+    return response.choices[0].message.content
+
+
+def extract_information_from_mind_map_data(openai_client_ object, data: dict) -> None:
+    logger.info(f"Extracting information from mind map data...")
+    user_prompt = PROMPTS["inspector_of_mind_map"].format(mind_map_data=data)
+    response = openai_client.chat.completions.create(
+        model="gpt-4",
+        messages=[
+            {"role": "system", "content": PROMPTS["inspector_of_mind_map"]},
+            {"role": "user", "content": user_prompt},
+        ],
+    )
+    return response.choices[0].message.content
