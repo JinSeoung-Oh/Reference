39e6534a937c3207d53a09d31a3b61aaa059aa4b
Jin-Seoung <jinseoungoh@gmail.com>
2024-04-22 09:41:33 +0900

    Create LLM_agents.py

---
+++ text/Agent/LLM_agents.py
@@ -0,0 +1,87 @@
+## https://medium.com/@raunak-jain/path-to-production-for-llm-agents-f0a9cafd2398
+
+import re
+from langchain_core.prompts import ChatPromptTemplate
+
+# Regex to match expressions of the form E#... = ...[...]
+regex_pattern = r"Plan:\s*(.+)\s*(#E\d+)\s*=\s*(\w+)\s*\[([^\]]+)\]"
+prompt_template = ChatPromptTemplate.from_messages([("user", prompt)])
+planner = prompt_template | model
+
+# Planner - find sample
+def get_plan(state: ReWOO):
+    task = state["task"]
+    result = planner.invoke({"task": task})
+    # Find all matches in the sample text
+    matches = re.findall(regex_pattern, result.content)
+    return {"steps": matches, "plan_string": result.content}
+
+# Worker
+def _get_current_task(state: ReWOO):
+    if state["results"] is None:
+        return 1
+    if len(state["results"]) == len(state["steps"]):
+        return None
+    else:
+        return len(state["results"]) + 1
+
+def tool_execution(state: ReWOO):
+    """Worker node that executes the tools of a given plan."""
+    _step = _get_current_task(state)
+    _, step_name, tool, tool_input = state["steps"][_step - 1]
+    _results = state["results"] or {}
+    for k, v in _results.items():
+        tool_input = tool_input.replace(k, v)
+    if tool == "Google":
+        result = search.invoke(tool_input)
+    elif tool == "LLM":
+        result = model.invoke(tool_input)
+    else:
+        raise ValueError
+    _results[step_name] = str(result)
+    return {"results": _results}
+
+# Solver
+solve_prompt = """Solve the following task or problem. To solve the problem, we have made step-by-step Plan and \
+retrieved corresponding Evidence to each Plan. Use them with caution since long evidence might \
+contain irrelevant information.
+
+{plan}
+
+Now solve the question or task according to provided Evidence above. Respond with the answer
+directly with no extra words.
+
+Task: {task}
+Response:"""
+
+def solve(state: ReWOO):
+    plan = ""
+    for _plan, step_name, tool, tool_input in state["steps"]:
+        _results = state["results"] or {}
+        for k, v in _results.items():
+            tool_input = tool_input.replace(k, v)
+            step_name = step_name.replace(k, v)
+        plan += f"Plan: {_plan}\n{step_name} = {tool}[{tool_input}]"
+    prompt = solve_prompt.format(plan=plan, task=state["task"])
+    result = model.invoke(prompt)
+    return {"result": result.content}
+
+# built as a graph
+from langgraph.graph import StateGraph, END
+
+graph = StateGraph(ReWOO)
+graph.add_node("plan", get_plan)
+graph.add_node("tool", tool_execution)
+graph.add_node("solve", solve)
+graph.add_edge("plan", "tool")
+graph.add_edge("solve", END)
+graph.add_conditional_edges("tool", _route)
+graph.set_entry_point("plan")
+
+app = graph.compile()
+
+
+
+
+
+
