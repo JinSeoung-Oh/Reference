ca9abd900523a620fae92e8ae30a65a4f02efcf1
Jin-Seoung <jinseoungoh@gmail.com>
2024-04-26 12:16:33 +0900

    Create Fine_tune_Llama_3_with_ORPO.py

---
+++ Tuning/Fine_tuning/Fine_tune_Llama_3_with_ORPO.py
@@ -0,0 +1,120 @@
+"""
+From https://towardsdatascience.com/fine-tune-llama-3-with-orpo-56cfab2f9ada
+
+ORPO offers an solution by combining instruction tuning and preference alignment into a single, 
+monolithic training process.
+ORPO modifies the standard language modeling objective, combining the negative log-likelihood loss with an odds ratio (OR) term.
+This OR loss weakly penalizes rejected responses while strongly rewarding preferred ones,
+allowing the model to simultaneously learn the target task and align with human preferences.
+
+"""
+
+! pip install -U transformers datasets accelerate peft trl bitsandbytes wandb
+
+import gc
+import os
+
+import torch
+import wandb
+from datasets import load_dataset
+from google.colab import userdata
+from peft import LoraConfig, PeftModel, prepare_model_for_kbit_training
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    TrainingArguments,
+    pipeline,
+)
+from trl import ORPOConfig, ORPOTrainer, setup_chat_format
+wb_token = userdata.get('wandb')
+wandb.login(key=wb_token)
+
+if torch.cuda.get_device_capability()[0] >= 8:
+    !pip install -qqq flash-attn
+    attn_implementation = "flash_attention_2"
+    torch_dtype = torch.bfloat16
+else:
+    attn_implementation = "eager"
+    torch_dtype = torch.float16
+
+# Model
+base_model = "meta-llama/Meta-Llama-3-8B"
+new_model = "OrpoLlama-3-8B"
+
+# QLoRA config
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch_dtype,
+    bnb_4bit_use_double_quant=True,
+)
+
+# LoRA config
+peft_config = LoraConfig(
+    r=16,
+    lora_alpha=32,
+    lora_dropout=0.05,
+    bias="none",
+    task_type="CAUSAL_LM",
+    target_modules=['up_proj', 'down_proj', 'gate_proj', 'k_proj', 'q_proj', 'v_proj', 'o_proj']
+)
+
+# Load tokenizer
+tokenizer = AutoTokenizer.from_pretrained(base_model)
+
+# Load model
+model = AutoModelForCausalLM.from_pretrained(
+    base_model,
+    quantization_config=bnb_config,
+    device_map="auto",
+    attn_implementation=attn_implementation
+)
+model, tokenizer = setup_chat_format(model, tokenizer)
+model = prepare_model_for_kbit_training(model)
+
+dataset_name = "mlabonne/orpo-dpo-mix-40k"
+dataset = load_dataset(dataset_name, split="all")
+dataset = dataset.shuffle(seed=42).select(range(1000))
+
+def format_chat_template(row):
+    row["chosen"] = tokenizer.apply_chat_template(row["chosen"], tokenize=False)
+    row["rejected"] = tokenizer.apply_chat_template(row["rejected"], tokenize=False)
+    return row
+
+dataset = dataset.map(
+    format_chat_template,
+    num_proc= os.cpu_count(),
+)
+dataset = dataset.train_test_split(test_size=0.01)
+
+orpo_args = ORPOConfig(
+    learning_rate=8e-6,
+    beta=0.1,
+    lr_scheduler_type="linear",
+    max_length=1024,
+    max_prompt_length=512,
+    per_device_train_batch_size=2,
+    per_device_eval_batch_size=2,
+    gradient_accumulation_steps=4,
+    optim="paged_adamw_8bit",
+    num_train_epochs=1,
+    evaluation_strategy="steps",
+    eval_steps=0.2,
+    logging_steps=1,
+    warmup_steps=10,
+    report_to="wandb",
+    output_dir="./results/",
+)
+
+trainer = ORPOTrainer(
+    model=model,
+    args=orpo_args,
+    train_dataset=dataset["train"],
+    eval_dataset=dataset["test"],
+    peft_config=peft_config,
+    tokenizer=tokenizer,
+)
+
+trainer.train()
+trainer.save_model(new_model)
