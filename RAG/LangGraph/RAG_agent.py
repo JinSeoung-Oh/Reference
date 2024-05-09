f4f0b409c69915b7072306320ff16db4dc936ab0
Jin-Seoung <jinseoungoh@gmail.com>
2024-05-09 10:26:41 +0900

    Update RAG_agent.py

--- RAG/LangGraph/RAG_agent.py
+++ RAG/LangGraph/RAG_agent.py
@@ -1,3 +1,5 @@
+## From https://medium.com/the-ai-forum/build-a-reliable-rag-agent-using-langgraph-2694d55995cd
+
 ! pip install -U langchain-nomic langchain_community tiktoken langchainhub chromadb langchain langgraph tavily-python gpt4all fastembed langchain-groq 
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
