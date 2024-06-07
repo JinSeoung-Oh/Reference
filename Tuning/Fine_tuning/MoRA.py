b970c2a6f6d3b6d9819c5041f5dd173780996b62
Jin-Seoung <jinseoungoh@gmail.com>
2024-06-07 11:03:33 +0900

    Update MoRA.py

--- Tuning/Fine_tuning/MoRA.py
+++ Tuning/Fine_tuning/MoRA.py
@@ -79,3 +79,48 @@ like Low-Rank Adaptation (LoRA) and now, Matrix-based Rank Adaptation (MoRA).
 
 
 """
+
+!pip install -e ./peft-mora
+
+from peft import LoraConfig, get_peft_model
+config = LoraConfig(
+    use_mora=True, # enable mora
+    mora_type=1, # type 1 refer to Eq. 6, type 6 (RoPE based) for small ranks refer to Eq. 9 in paper.
+    r=lora_r, # lora rank here, we will calculate corresponding $\hat{r}$ in MoRA
+    target_modules=lora_target_modules,
+    lora_dropout=lora_dropout,
+    task_type="CAUSAL_LM",
+    **kwargs,
+)
+model = get_peft_model(model, config)
+
+
+## Examples
+# fine-tuning MetaMath with MoRA
+RANK=8
+deepspeed --num_gpus=8 --num_nodes=2 train.py \
+           --base_model <LLAMA-2> --micro_batch_size 4\
+            --wandb_run_name mora_math_r8 --lora_target_modules q_proj,k_proj,v_proj,o_proj,gate_proj,down_proj,up_proj \
+            --num_epochs 3 --deepspeed ds.config --wandb_project lora-math --lora_r $RANK --batch_size 128 \
+            --data_path meta-math/MetaMath \
+            --save_steps 3000 \
+            --learning_rate 3e-4 --mora_type 6 \
+            --logging_steps 5  --use_bf16  --use_16bit --use_mora
+
+deepspeed --num_gpus=8 --num_nodes=4 train.py \
+        --micro_batch_size 16 --wandb_run_name mora-pretrain250m-r128 \
+        --num_epochs 1 --wandb_project lora-pretrain --batch_size 1024 \
+        --data_path <processed C4> --logging_steps 1 \
+        --lora_target_modules q_proj,k_proj,v_proj,o_proj,gate_proj,down_proj,up_proj \
+        --lora_r 128 --lora_alpha 64 --warmup_steps 1000  \
+        --force_tqdm_update --lr_scheduler_type cosine \
+        --max_steps 10000 --pretrain 250m \
+        --train_embhead --learning_rate 5e-4 \
+        --use_mora --use_relora --use_relora_step 2000  # ReMoRA merge per 2000 steps
+
+
+
+
+
+
+
