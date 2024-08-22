11a1b811eb732df37c290395e1b73872af6945c7
Jin-Seoung <jinseoungoh@gmail.com>
2024-08-22 12:39:52 +0900

    Create Llama_3_1_with_Unsloth.py

---
+++ Tuning/Fine_tuning/Llama_3_1_with_Unsloth.py
@@ -0,0 +1,117 @@
+## From https://medium.com/towards-data-science/fine-tune-llama-3-1-ultra-efficiently-with-unsloth-7196c7165bab
+
+!pip install "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git"
+!pip install --no-deps "xformers<0.0.27" "trl<0.9.0" peft accelerate bitsandbytes
+
+import torch
+from trl import SFTTrainer
+from datasets import load_dataset
+from transformers import TrainingArguments, TextStreamer
+from unsloth.chat_templates import get_chat_template
+from unsloth import FastLanguageModel, is_bfloat16_supported
+
+### Load LLM model
+max_seq_length = 2048
+model, tokenizer = FastLanguageModel.from_pretrained(
+    model_name="unsloth/Meta-Llama-3.1-8B-bnb-4bit",
+    max_seq_length=max_seq_length,
+    load_in_4bit=True,
+    dtype=None,
+)
+
+### Load rsLoRA model - Rank-Stabilized LoRA (rsLoRA), which modifies the scaling factor of LoRA adapters to be proportional to 1/√r instead of 1/r. 
+model = FastLanguageModel.get_peft_model(
+    model,
+    r=16,
+    lora_alpha=16,
+    lora_dropout=0,
+    target_modules=["q_proj", "k_proj", "v_proj", "up_proj", "down_proj", "o_proj", "gate_proj"], 
+    use_rslora=True,
+    use_gradient_checkpointing="unsloth"
+)
+
+## Format
+"""
+<|im_start|>system
+You are a helpful assistant, who always provide explanation. Think like you are answering to a five year old.<|im_end|>
+<|im_start|>user
+Remove the spaces from the following sentence: It prevents users to suspect that there are some hidden products installed on theirs device.
+<|im_end|>
+<|im_start|>assistant
+Itpreventsuserstosuspectthattherearesomehiddenproductsinstalledontheirsdevice.<|im_end|>
+"""
+
+tokenizer = get_chat_template(
+    tokenizer,
+    mapping={"role": "from", "content": "value", "user": "human", "assistant": "gpt"},
+    chat_template="chatml",
+)
+
+def apply_template(examples):
+    messages = examples["conversations"]
+    text = [tokenizer.apply_chat_template(message, tokenize=False, add_generation_prompt=False) for message in messages]
+    return {"text": text}
+
+dataset = load_dataset("mlabonne/FineTome-100k", split="train")
+dataset = dataset.map(apply_template, batched=True)
+
+trainer=SFTTrainer(
+    model=model,
+    tokenizer=tokenizer,
+    train_dataset=dataset,
+    dataset_text_field="text",
+    max_seq_length=max_seq_length,
+    dataset_num_proc=2,
+    packing=True,
+    args=TrainingArguments(
+        learning_rate=3e-4,
+        lr_scheduler_type="linear",
+        per_device_train_batch_size=4,
+        gradient_accumulation_steps=4,
+        num_train_epochs=1,
+        fp16=not is_bfloat16_supported(),
+        bf16=is_bfloat16_supported(),
+        logging_steps=1,
+        optim="adamw_8bit",
+        weight_decay=0.01,
+        warmup_steps=10,
+        output_dir="output",
+        seed=0,
+    ),
+)
+
+trainer.train()
+
+
+
+### Inference
+model = FastLanguageModel.for_inference(model)
+
+messages = [
+    {"from": "human", "value": "Is 9.11 larger than 9.9?"},
+]
+inputs = tokenizer.apply_chat_template(
+    messages,
+    tokenize=True,
+    add_generation_prompt=True,
+    return_tensors="pt",
+).to("cuda")
+
+text_streamer = TextStreamer(tokenizer)
+
+
+### Save - There are three save methods in Unsloth: lora to only save the adapters, and merged_16bit/merged_4bit to merge the adapters with the model in 16-bit/ 4-bit precision.
+model.save_pretrained_merged("model", tokenizer, save_method="merged_16bit")
+model.push_to_hub_merged("mlabonne/FineLlama-3.1-8B", tokenizer, save_method="merged_16bit")
+
+### Unsloth also allows you to directly convert your model into GGUF format
+quant_methods = ["q2_k", "q3_k_m", "q4_k_m", "q5_k_m", "q6_k", "q8_0"]
+for quant in quant_methods:
+    model.push_to_hub_gguf("mlabonne/FineLlama-3.1-8B-GGUF", tokenizer, quant)
+
+
+
+
+_ = model.generate(input_ids=inputs, streamer=text_streamer, max_new_tokens=128, use_cache=True)
+
+
